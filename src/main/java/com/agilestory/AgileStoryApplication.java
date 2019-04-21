97cb09a278a6244d0929796898e4f646f241ddcf
Shemar Anderson <anderson.shemar17@gmail.com>
2019-04-21 00:35:30 -0500

    create back-end skeleton

---
+++ src/main/java/com/agilestory/AgileStoryApplication.java
@@ -0,0 +1,13 @@
+package com.agilestory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileStoryApplication.class, args);
+	}
+
+}
