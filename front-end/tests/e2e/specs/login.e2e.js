d67a43ae5c55210b28d145ea8f3c2d07519edc2e
Shemar Anderson <anderson.shemar17@gmail.com>
2019-04-21 22:06:38 -0500

    create front-end skeleton

---
+++ front-end/tests/e2e/specs/login.e2e.js
@@ -0,0 +1,12 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'login test': browser => {
+    browser
+      .url(process.env.VUE_DEV_SERVER_URL + 'login')
+      .waitForElementVisible('#app', 5000)
+      .assert.containsText('h1', 'AgileStory')
+      .end()
+  }
+}
